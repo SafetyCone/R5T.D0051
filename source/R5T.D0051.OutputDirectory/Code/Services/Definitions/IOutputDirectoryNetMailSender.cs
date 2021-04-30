9af2749787f144b0129d4f393e35a2300670ab68
David Coats <david@getsafetycone.com>
2021-04-30 16:10:02 -0700

    Add the R5T.D0051.OutputDirectory library project with IOutputDirectoryNetMailSender service implementation.

---
+++ source/R5T.D0051.OutputDirectory/Code/Services/Definitions/IOutputDirectoryNetMailSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using R5T.D0051.Net.Mail;
+
+
+namespace R5T.D0051.OutputDirectory
+{
+    public interface IOutputDirectoryNetMailSender : INetMailEmailSender;
+    {
+    }
+}
---
+++ source/R5T.D0051.OutputDirectory/Code/Services/Implementations/EmailSender.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using R5T.D0055;
+
+
+namespace R5T.D0051.OutputDirectory
+{
+    public class EmailSender : IOutputDirectoryNetMailSender
+    {
+        private IEmailOutputDirectoryPathProvider EmailOutputDirectoryPathProvider { get; }
+        private ILogger Logger { get; }
+
+
+        public EmailSender(
+            IEmailOutputDirectoryPathProvider emailOutputDirectoryPathProvider,
+            ILogger<EmailSender> logger)
+        {
+            this.EmailOutputDirectoryPathProvider = emailOutputDirectoryPathProvider;
+            this.Logger = logger;
+        }
+
+        public async Task Send(MailMessage emailMessage)
+        {
+            var emailOutputDirectoryPath = await this.EmailOutputDirectoryPathProvider.GetEmailOutputDirectoryPath();
+
+            try
+            {
+                using (var smtpClient = new SmtpClient())
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailOutputDirectoryPath;
+
+                    await smtpClient.SendMailAsync(emailMessage);
+
+                    this.Logger.LogInformation("Sent email to output directory.");
+                }
+            }
+            catch (Exception exception)
+            {
+                this.Logger.LogError(exception, "Failed to send email to output directory.");
+
+                throw exception; // Re-throw.
+            }
+        }
+    }
+}
