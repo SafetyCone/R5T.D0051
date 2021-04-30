01ccdc615e67e2647e3da6e2a6c588e8f772739a
David Coats <david@getsafetycone.com>
2021-04-30 16:15:08 -0700

    Remove stray typo.

--- source/R5T.D0051.OutputDirectory/Code/Services/Definitions/IOutputDirectoryNetMailSender.cs
+++ source/R5T.D0051.OutputDirectory/Code/Services/Definitions/IOutputDirectoryNetMailSender.cs
@@ -5,7 +5,7 @@ using R5T.D0051.Net.Mail;
 
 namespace R5T.D0051.OutputDirectory
 {
-    public interface IOutputDirectoryNetMailSender : INetMailEmailSender;
+    public interface IOutputDirectoryNetMailSender : INetMailEmailSender
     {
     }
 }
