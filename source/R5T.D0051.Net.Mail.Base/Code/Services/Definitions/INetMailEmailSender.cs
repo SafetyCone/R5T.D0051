bee14048e2c86c510af1ce46d28c5ef055f16713
David Coats <david@getsafetycone.com>
2021-04-30 12:48:26 -0700

    Quick change to documentation

--- source/R5T.D0051.Net.Mail.Base/Code/Services/Definitions/INetMailEmailSender.cs
+++ source/R5T.D0051.Net.Mail.Base/Code/Services/Definitions/INetMailEmailSender.cs
@@ -5,7 +5,7 @@ using System.Net.Mail;
 namespace R5T.D0051.Net.Mail
 {
     /// <summary>
-    /// Service for sending <see cref="System.Net.Mail.MailMessage"/> emails.
+    /// Service for sending <see cref="MailMessage"/> emails.
     /// </summary>
     /// <remarks>
     public interface INetMailEmailSender<TEmailMessage> : IEmailSender<MailMessage>
