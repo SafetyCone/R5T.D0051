d9137be3cc133926db89880a08af5bc292758602
David Coats <david@getsafetycone.com>
2021-04-30 11:36:43 -0700

    Add R5T.D0051.Base and R5T.D0051.Net.Mail service definition libraries

---
+++ source/R5T.D0051.Base/Code/Services/Definitions/IEmailSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0051
+{
+    /// <summary>
+    /// Service for sending emails, generically typed for the email messsage representation class.
+    /// </summary>
+    /// <remarks>
+    /// Sending only.
+    /// </remarks>
+    public interface IEmailSender<TEmailMessage>
+    {
+        Task Send(TEmailMessage emailMessage);
+    }
+}
---
+++ source/R5T.D0051.Net.Mail/Code/Services/Definitions/INetMailEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Mail;
+
+
+namespace R5T.D0051.Net.Mail
+{
+    /// <summary>
+    /// Service for sending <see cref="System.Net.Mail.MailMessage"/> emails.
+    /// </summary>
+    /// <remarks>
+    public interface INetMailEmailSender<TEmailMessage> : IEmailSender<MailMessage>
+    {
+    }
+}
